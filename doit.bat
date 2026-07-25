d2415c5495efa436159476c26a3cd58ecd744b7c
Jay Ongg <swingtempo>
2026-07-24 22:34:54 -0700

    publish future ones

--- doit.bat
+++ doit.bat
@@ -1,4 +1,4 @@
 rem make sure you have installed Ruby: e.g.  winget install -e RubyInstallerTeam.RubyWithDevKit.4.0
 rem and have run `gem install bundler jekyll`
 rem and run "bundle install"
-bundle exec jekyll serve --future
+bundle exec jekyll serve
